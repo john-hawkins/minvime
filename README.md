53716b32e325a7412a9cc12ee0fd2d7c2e678224
John Hawkins <john.hawkins@john.hawkins.MacBook>
2019-12-11 21:25:10 +1100

    Reshaping application, preparing for alternative approaches

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
-Estimate Minimum Required Model Performance
+MinViME - Minimum Viable Model Estimator 
 -------------------------------------------
 
-In many cases it is worthwhile understanding how accurate a
+For many business problems it is worthwhile understanding how accurate a
 machine learning model would need to be before you try and build it.
 
 This information could be useful in prioritising projects or determing the
-quantity and quality of data that will be needed. As extremely accurate models
+quantity and quality of data that will be needed. Extremely accurate models
 for complicated problems typically require large amounts of data.
 
 This application is a tool that allows you to estimate
 what the minimum performance characteristics would need to be.
-It does this in terms of the ROC plot charcateristics that satisfy the constraints
+
+The first version will be restricted to binary classification problems only.
+
+It will make the estimate in terms of the ROC plot charcateristics that satisfy the constraints
 provided as a cost/benefit payoff matrix, the required ROI and information about the 
 volume and frequency of the event you are predicting.
 
--- app.py
+++ app.py
@@ -28,6 +28,28 @@ def index():
     return render_template("index.html",
        tp=tp, fp=fp, tn=tn, fn=fn, cases=cases, baserate=baserate, minroi=minroi)
 
+# ###################################################################################
+# Cost Benefit Page
+@app.route('/payoff_matrix', methods = ['POST', 'GET'])
+def payoff_matrix():
+    tp = 5000
+    fp = -50
+    tn = 0
+    fn = 0
+    minroi = 10000
+    cases = 10000
+    baserate = 0.001
+    if request.method == 'POST':
+       if 'tp' in request.values:
+          tp = float(request.form["tp"])
+          fp = float(request.form["fp"])
+          tn = float(request.form["tn"])
+          fn = float(request.form["fn"])
+          cases = float(request.form["cases"])
+          baserate = float(request.form["baserate"])
+
+    return render_template("payoff_matrix.html",
+       tp=tp, fp=fp, tn=tn, fn=fn, cases=cases, baserate=baserate, minroi=minroi)
 
 # ###################################################################################
 @app.route('/analyse', methods = ['POST', 'GET'])
@@ -59,6 +81,23 @@ def analyse():
     return render_template("analyse.html", auc=auc, precision=prec, recall=recall,  
         tp=tp, fp=fp, tn=tn, fn=fn, cases=cases, baserate=baserate, minroi=minroi)
 
+# ###################################################################################
+# Intervention Page
+@app.route('/intervention')
+def intervention():
+        return render_template("intervention.html")
+
+# ###################################################################################
+# 
+@app.route('/proportional', methods = ['POST', 'GET'])
+def proportional():
+        return render_template("proportional.html")
+
+# ###################################################################################
+# 
+@app.route('/thresholded')
+def thresholded():
+        return render_template("thresholded.html")
 
 # ###################################################################################
 # About Page
