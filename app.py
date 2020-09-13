f1d168528cfa11f01044bfb123b3805935ae6dbb
john-hawkins <hawkins.john.c@gmail.com>
2020-09-13 12:47:27 +1000

    beginning refactor for packaging

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, flash, request, render_template
-import src.estimator as esti # The file src/estimator.py 
-import src.estimator_for_regression as r_esti # The src/estimator_for_regression.py
+from flask import Flask, flash, request, redirect, render_template, url_for
 from werkzeug.utils import secure_filename
+
 from pathlib import Path
 import os
 
-app = Flask(__name__)
+import minvime.estimator as esti # The file minvime/estimator.py
+
+import minvime.estimator_for_regression as r_esti # The minvime/estimator_for_regression.py
 
 UPLOAD_FOLDER = './uploads'
 
@@ -13,6 +14,9 @@ ALLOWED_EXTENSIONS = set(['csv'])
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+app = Flask(__name__)
+
 
 # ###################################################################################
 # Index Page
@@ -59,8 +63,8 @@ def analyse():
         fp = -50
         tn = 0
         fn = 0
-        minroi = 10000 
-        cases = 10000 
+        minroi = 10000
+        cases = 10000
         baserate = 0.001
 
     auc, prec, recall = esti.estimate_binary_model_requirements(
@@ -70,7 +74,7 @@ def analyse():
     prec = round(prec, 3)
     recall = round(recall, 3)
 
-    return render_template("analyse.html", auc=auc, precision=prec, recall=recall,  
+    return render_template("analyse.html", auc=auc, precision=prec, recall=recall,
         tp=tp, fp=fp, tn=tn, fn=fn, cases=cases, baserate=baserate, minroi=minroi)
 
 # ###################################################################################
@@ -87,14 +91,14 @@ def intervention():
     payback = -400
 
 
-    return render_template("intervention.html", 
+    return render_template("intervention.html",
         minroi=minroi, cases=cases, baserate=baserate,
         cost=cost, payoff=payoff, payback=payback,
         succrate=succrate, backfire=backfire
     )
 
 # ###################################################################################
-# Intervention Page 
+# analyse Intervention Page
 @app.route('/analyse_intervention', methods = ['POST', 'GET'])
 def analyse_intervention():
     if 'payoff' in request.values:
@@ -127,15 +131,15 @@ def analyse_intervention():
     prec = round(prec, 3)
     recall = round(recall, 3)
 
-    return render_template("analyse_intervention.html", 
+    return render_template("analyse_intervention.html",
         auc=auc, precision=prec, recall=recall,
         minroi=minroi, cases=cases, baserate=baserate,
         cost=cost, payoff=payoff, payback=payback,
         succrate=succrate, backfire=backfire
     )
- 
+
 # ###################################################################################
-# 
+#
 @app.route('/proportional', methods = ['POST', 'GET'])
 def proportional():
     minroi = 10000
@@ -147,7 +151,7 @@ def proportional():
     min = 0
     max = 500
 
-    return render_template("proportional.html", 
+    return render_template("proportional.html",
         minroi=minroi, cases=cases, mean=mean, max=max, min=min,
         pred_value=pred_value, under_pred=under_pred, over_pred=over_pred
     )
@@ -167,13 +171,13 @@ def analyse_proportional():
     min = float(request.form["min"])
     max = float(request.form["max"])
 
-    # CHECK FOR THE SAMPLE FILE 
+    # CHECK FOR THE SAMPLE FILE
     sample_file = False
     if 'file' in request.files:
         file = request.files['file']
         if file.filename != '':
             sample_file = True
-  
+
     if sample_file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         filepath = os.path.join(UPLOAD_FOLDER, filename)
@@ -182,27 +186,27 @@ def analyse_proportional():
     else:
         dist, message = r_esti.produce_distribution_sample(mean=mean, max=max, min=min)
 
-    if len(dist) == 0: 
+    if len(dist) == 0:
         return render_template("error.html", message=message, link="proportional.html")
 
     rmse, mape, mae = r_esti.estimate_model_requirements_proportional(
-        dist=dist, cases=cases, 
-        pred_value=pred_value, under_pred=under_pred, under_pred_unit=under_pred_unit, 
-        over_pred=over_pred, over_pred_unit=over_pred_unit, minroi=minroi 
+        dist=dist, cases=cases,
+        pred_value=pred_value, under_pred=under_pred, under_pred_unit=under_pred_unit,
+        over_pred=over_pred, over_pred_unit=over_pred_unit, minroi=minroi
     )
-
     rmse = round(rmse, 3)
     mape = round(mape, 3)
     mae = round(mae, 3)
-    
-    return render_template("analyse_proportional.html", 
-        rmse=rmse, mae=mae, mape=mape, minroi=minroi, cases=cases, 
-        mean=mean, max=max, min=min, 
+
+    return render_template("analyse_proportional.html",
+        rmse=rmse, mae=mae, mape=mape, minroi=minroi, cases=cases,
+        mean=mean, max=max, min=min,
         pred_value=pred_value, under_pred=under_pred, over_pred=over_pred
     )
- 
+
+
 # ###################################################################################
-# 
+#
 @app.route('/thresholded')
 def thresholded():
     minroi = 10000
@@ -218,10 +222,9 @@ def thresholded():
 
     return render_template("thresholded.html",
         minroi=minroi, cases=cases, mean=mean, max=max, min=min,
-        pred_value=pred_value, under_pred=under_pred, under_pred_threshold=under_pred_threshold, 
+        pred_value=pred_value, under_pred=under_pred, under_pred_threshold=under_pred_threshold,
         over_pred=over_pred, over_pred_threshold=over_pred_threshold
     )
-
 
 # ###################################################################################
 # Analyse thresholded Costs for a Regression Problem
@@ -259,7 +262,7 @@ def analyse_thresholded():
         return render_template("error.html", message=message, link="thresholded.html")
 
     rmse, mape, mae = r_esti.estimate_model_requirements_thresholded(
-        dist=dist, cases=cases, pred_value=pred_value, 
+        dist=dist, cases=cases, pred_value=pred_value,
         under_pred=under_pred, under_pred_unit=under_pred_unit, under_pred_threshold=under_pred_threshold,
         over_pred=over_pred, over_pred_unit=over_pred_unit, over_pred_threshold=over_pred_threshold, minroi=minroi
     )
@@ -275,9 +278,6 @@ def analyse_thresholded():
         over_pred=over_pred, over_pred_threshold=over_pred_threshold
     )
 
-
-
-
 # ###################################################################################
 # About Page
 @app.route('/about')
@@ -285,9 +285,10 @@ def about():
         return render_template("about.html")
 
 
-# With debug=True, Flask server will auto-reload 
+# ###################################################################################
+# With debug=True, Flask server will auto-reload
 # when there are code changes
 if __name__ == '__main__':
-	app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True)
 
 
