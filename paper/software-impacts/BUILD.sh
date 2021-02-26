9f993f0dc14721244cd21da6683674c00dbfd1d2
John Hawkins <john.hawkins@john.hawkins.MacBook>
2021-02-27 10:13:02 +1100

    Starting Software Impacts paper

---
+++ paper/software-impacts/BUILD.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+bibtex software_impacts_minvime
+
+bibtex software_impacts_minvime
+
+pdflatex software_impacts_minvime.tex 
+
+rm software_impacts_minvime.aux 
+rm software_impacts_minvime.out 
+rm software_impacts_minvime.log 
+rm software_impacts_minvime.spl
+rm software_impacts_minvime.bbl
+rm software_impacts_minvime.blg
+ 
