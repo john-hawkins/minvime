5ab4b608fcc202bf694bdcded916ae17a5add13f
John Hawkins <john.hawkins@john.hawkins.MacBook>
2021-03-22 21:34:19 +1100

    Adding clean-up to paper build

--- paper/software-impacts/BUILD.sh
+++ paper/software-impacts/BUILD.sh
@@ -6,10 +6,10 @@ bibtex software_impacts_minvime
 
 pdflatex software_impacts_minvime.tex 
 
-#rm software_impacts_minvime.aux 
-#rm software_impacts_minvime.out 
-#rm software_impacts_minvime.log 
-#rm software_impacts_minvime.spl
-#rm software_impacts_minvime.bbl
-#rm software_impacts_minvime.blg
+rm software_impacts_minvime.aux 
+rm software_impacts_minvime.out 
+rm software_impacts_minvime.log 
+rm software_impacts_minvime.spl
+rm software_impacts_minvime.bbl
+rm software_impacts_minvime.blg
  
