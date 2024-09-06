1b1adb08698be6c74a5209079a353f835b071009
John <hawkins.john.c@gmail.com>
2024-09-06 18:27:34 +1000

    Slight changes for dockerised version

--- minvime-runner.py
+++ minvime-runner.py
@@ -4,6 +4,7 @@
 """Convenience wrapper for running minvime directly from source tree."""
  
 from minvime.minvime import main
+from minvime.minvime import app
  
 if __name__ == '__main__':
     main()
